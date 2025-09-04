6a9a6ff8d840a37e3d86a395afa332f557ef2235
BernardAvelino <bernardz.dev@gmail.com>
2025-09-04 14:35:21 -0300

    Add files via upload

---
+++ conta.py
@@ -0,0 +1,10 @@
+def multiplica ():
+    return hp * 365
+
+hp = int(input("Quanto anos você tem?: "))
+
+hr = input("Qual seu nome?: ")
+
+print(f"Seu nome é {hr}: ")
+
+print(f"Você ja viveu: {multiplica()} dias!")
